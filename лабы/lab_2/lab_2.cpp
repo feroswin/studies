f1be464b4e66feeaeb63b514b679085564cb6242
feroswin <vitalkaivanov009@mail.ru>
2021-09-24 18:44:06 +0300

    загрузка 2 лабы

---
+++ лабы/lab_2/lab_2.cpp
@@ -0,0 +1,27 @@
+﻿/*Даны действительные числа а, b, с.Удвоить эти числа, если a≥b≥c, и заменить их абсолютными
+значениями, если это не так.*/
+
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	float a, b, c;
+	cout << "Введите a,b,c";
+	cin >> a >> b >> c;
+	if (a >= b && b >= c && c <= a && c <= b) {
+		a *= 2;
+		b *= 2;
+		c *= 2;
+		cout << "a=" << a << "\tb=" << b << "\tc=" << c;
+	}
+	else {
+		a = abs(a);
+		b = abs(b);
+		c = abs(c);
+		cout << "a=" << a << "\tb=" << b << "\tc=" << c;
+	}
+}
+
