9abfbe6bfb2da67703a496e57a927ce649c8eb6e
feroswin <vitalkaivanov009@mail.ru>
2021-09-24 12:09:51 +0300

    загрузка 1 лабы

--- учеба/js/main.js
+++ учеба/js/main.js
@@ -2,14 +2,7 @@ let start_game = "default", card_player1, card_player2,
 deposit, bet, balance, choice;
 let coloda = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
-function func_check_start_game() {
-    while(start_game != "y"){
-        start_game = prompt(`Введите 'y', если хотите начать игру,иначе 'n'`);
-        if (start_game == "y"){
-    break;
-        }
-    }
-}
+
 
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
@@ -22,34 +15,39 @@ function player_cards() {
     score = card_player1 + card_player2;
 }
 
-function choice_cards_player() {
-    choice = prompt(`Если вы хотите взять карту введите 'y', иначе 'n'`);
-    if (choice == 'y') {
-        while (choice == 'y') {
+function diller_cards() {
+    shuffle(coloda);
+    card_diller1 = coloda[0];
+    card_diller2 = coloda[1];
+    score_diller = card_diller1 + card_diller2;
+}
+
+function check_cards_diller() {
+    if (score_diller < 17) {
+        while (score_diller < 17) {
+            alert(`Диллер берет карту`);
             shuffle(coloda);
-            card_player3 = coloda[2];
-            score += card_player3;
-            if (score > 21 && choice == 'y') {                                 //32 строка Здесь должна быть функция проверки на больше чем 21 или нет
-                alert(`Вы проиграли набрав ${score}`);
+            card_diller3 = coloda[3];
+            score_diller += card_diller3;
+            if (score_diller > score) {
+                alert(`Диллер проиграл набрав ${score_diller}очков. Вы выиграли набрав ${score}очков`);
         break;
-            }else if (score == 21 && choice == 'y') {
-                alert(`Ваша ставка сыграла. Вы получаете +  ${2*bet}`);
-                balance += 2*bet;
-                alert(`Ваш баланс равен ${balance}`);
-        break;
-            }
-            if (choice == 'n') {
-                alert(`Вы набрали ${score} очков и завершили игру`);
-        break;
+            }else if (score_diller == score) {
+                alert('Ничья');
+            }else {
+                alert(`Вы выиграли набрав ${score}очков`);
             }
         }
-    }else alert(`Вы набрали ${score} очков и завершили игру`);
+    }
 }
 
 
-
-
-func_check_start_game();
+while(start_game != "y"){
+    start_game = prompt(`Введите 'y', если хотите начать игру,иначе 'n'`);
+    if (start_game == "y"){
+break;
+    }
+}
 deposit = Number(prompt('Введите депозит'));
 balance = deposit;
 bet = Number(prompt('Введите сумму ставки'));
@@ -64,5 +62,28 @@ if (bet > deposit) {
 }
 alert('Вам выдаются карты');
 player_cards();
-alert(`У вас ${score} очков`);
-choice_cards_player();
+alert(`Вам выпадют карты ${card_player1} ${card_player2}, в сумме у вас ${score} очков`);
+alert('Диллеру выдаются карты');
+diller_cards();
+alert(`Диллеру выпадуют карты ${card_diller1} ?`);
+choice = prompt(`Если вы хотите взять карту введите 'y', иначе 'n'`);
+if (choice == 'y') {
+    while (choice == 'y') {
+        shuffle(coloda);
+        card_player3 = coloda[2];
+        score += card_player3;
+        alert(`Вам выпала карта ${card_player3}`);
+        alert(`У вас ${score} очков`);
+        if (score > 21 && choice == 'y') {
+            alert(`Вы проиграли набрав ${score}`);
+    break;
+        }else if (score > 21 && choice == 'n') {
+            alert(`Вы проиграли набрав ${score} очков`);
+    break;
+        }else {
+            choice = prompt(`Если вы хотите взять карту введите 'y', иначе 'n'`);
+        }
+    }
+}else {
+    alert(`Вы набрали ${score} очков и завершили игру`);
+}
