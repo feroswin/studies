85b8261bbc3c05dd35760c34d770f9ec7df337cd
feroswin <vitalkaivanov009@mail.ru>
2021-09-25 18:08:16 +0300

    загрузка 4 лабы

---
+++ лабы/lab_4/lab_4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+void main() 
+{
+	setlocale(LC_ALL, "RUS");
+	int a, k;
+	cout << "Введите количество долларов";
+	cin >> a;
+	if (a >= 11 && a <= 14) {
+		cout << a << " долларов";
+	}
+	else 
+	{
+		k = a % 10;
+		switch (k)
+		{
+		case 1:
+		{
+			cout << a << " доллар";
+			break;
+		}
+		case 2:
+		case 3:
+		case 4:
+		{
+			cout << a << " доллара";
+			break;
+		}
+		case 0:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		{
+			cout << a << " долларов";
+			break;
+		}
+		}
+	}
+}
