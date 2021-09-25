4d4a13024dfcbd41d5608e896722b76b722f8532
feroswin <vitalkaivanov009@mail.ru>
2021-09-25 17:03:40 +0300

    загрузка 3 лабы

---
+++ лабы/lab_3/lab_3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+void main() 
+{
+	setlocale(LC_ALL, "RUS");
+	float x, y;
+	cout << "Введите x,y";
+	cin >> x >> y;
+	if ((x <= 0 && y <= 0 && y >= -x - 6)||(x >= 0 && y >= 0 && y <= -x + 6)) {
+		cout << "Точка попадает в закрашенную область";
+	}
+	else
+	{
+		cout << "Точка не попадает в закрашенную область";
+	}
+}
