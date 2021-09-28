5b77d831446e607acb28d82454e96e5aa1e99105
feroswin <vitalkaivanov009@mail.ru>
2021-09-28 21:15:43 +0300

    загрузка задачи 7

---
+++ учеба/work_7/work_7.cpp
@@ -0,0 +1,25 @@
+﻿/* 
+	Необходимо суммировать все нечётные целые числа в диапазоне,
+	который введёт пользователь с клавиатуры.
+*/
+
+#include <iostream>
+#include <locale.h>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	int start, stop, sum = 0;
+	cout << "Введите начало диапазона" << endl;
+	cin >> start;
+	cout << "Введите конец диапазона" << endl;
+	cin >> stop;
+	for (start;start <= stop; start++)
+		{
+		if(start % 2 != 0 && start % start == 0)
+			{
+			sum += start;
+			cout << start << endl;
+			}
+		}
+}
