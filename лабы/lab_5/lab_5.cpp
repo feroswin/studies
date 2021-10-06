d916ff1c17520a6b89eb9d0c335061c206ef3145
feroswin <vitalkaivanov009@mail.ru>
2021-10-06 18:57:36 +0300

    загрузка 5 лабы

---
+++ лабы/lab_5/lab_5.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <locale.h>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	float Xn, Xk, dx, eps, x, s, f;
+	int i, n;
+
+	cout << "Введите Xn = " << endl;
+	cin >> Xn;
+	cout << "Введите Xk = " << endl;
+	cin >> Xk;
+	cout << "Введите шаг dx = " << endl;
+	cin >> dx;
+	cout << "Введите точность eps" << endl;
+	cin >> eps;
+	cout << "\n         x     S(x)       sin x       n\n";
+	n = 0;
+	for (x = Xn; x <= Xk; x += dx) {
+		s = 1;
+		for (f = 1, i = 2; fabs(f) > eps; i += 2) {
+			f = f * (-x * x / (i + 1) / i);
+			s += f;
+			n = i;
+		}
+		cout << "\n " << setprecision(4) << setw(10) << x
+			<< setprecision(4) << setw(10) << s
+			<< setprecision(4) << setw(10) << s
+			<< setw(10) << n;
+	}
+}
