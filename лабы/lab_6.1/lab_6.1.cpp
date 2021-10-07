acee00e77210e3714dd349c4548d55e67c86263e
feroswin <vitalkaivanov009@mail.ru>
2021-10-07 20:49:13 +0300

    интеграл по формуле трапеции к 6 лабе

---
+++ лабы/lab_6.1/lab_6.1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <locale.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	float a = 0, b = M_PI, y=0, h, x;
+	int n;
+	cout << "Введите n" << endl;
+	cin >> n;
+	h = (b - a) / n;
+	for (x = a + h; x < b; x += h)
+		y += (M_PI - x) * sin(x);
+	y *= h;
+	cout << "y= " << y << endl;
+}
