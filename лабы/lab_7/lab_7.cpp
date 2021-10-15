dc1caeb0c6fa3d0999f109028854dae2165278ec
feroswin <vitalkaivanov009@mail.ru>
2021-10-15 18:23:13 +0300

    7 лаба 100% окончание

--- лабы/lab_7/lab_7.cpp
+++ лабы/lab_7/lab_7.cpp
@@ -6,26 +6,30 @@
 #include <iostream>
 #include <locale.h>
 #include <math.h>
+#include <ctime>
 using namespace std;
 void main() 
 {
 	setlocale(LC_ALL, "RUS");
-	int i, n, k, max = 0,k1=0;
+	int i, n, max = 0;
 	float a[1000], s=0;
+	srand(time(0));
 
 	cout << "Введите кол-во элементов массива";
 	cin >> n;
 	for (i = 0; i < n; i++)
 	{
-		cout << "a[" << i << "]=";
-		cin >> a[i];                                             /* Ввод элементов массива до n */
+		a[i] = rand() % 9 - 1;
+		cout << "a[" << i << "]=" << a[i] << endl;                        /* Ввод элементов массива до n */
 	}
-
-	for(i = 0; i < n; i++)
-	{	
+	for (i = 0; i < n; i++)
+	{
 		if (fabs(a[i]) > fabs(a[max]))
-			max = i;                                           /*Номер максимального по модулю элемента массива*/
+			max = i;                                                      /*Номер максимального по модулю элемента массива*/
 	}
+	for (i = n - 1; a[i] > 0; i--)
+		s += a[i];                                                        /*Сумма элементов массива, расположенных после первого положительного элемента*/
+
 	cout << "\nНомер максимального по модулю элемента массива = " << max;
 	cout << "\nСумма после последнего положительного числа = " << s;
 }
