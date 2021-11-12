d8f6647fd0f9b9516292ed710abec09ae4e77dd0
feroswin <vitalkaivanov009@mail.ru>
2021-11-12 11:37:03 +0300

    Загрузка 12 лабы окончательная

--- лабы/lab_12/lab_12.cpp
+++ лабы/lab_12/lab_12.cpp
@@ -7,10 +7,10 @@
 #include <locale.h>
 #include <iomanip>
 using namespace std;
-void arrayNew(int kol, int M[])
+void arrayNew(int N, int M[])
 {
 	int i;
-	for (i = 0; i < kol; i++)
+	for (i = 0; i < N; i++)
 	{
 		M[i] = 10 - rand() % 30;
 		cout << setw(6) << M[i];
@@ -21,9 +21,10 @@ void arrayNew(int kol, int M[])
 }
 
 
-int arraySum(int S, int num, int A[] )
+int arraySum(int num, int A[])
 {
 	int i;
+	int S=0;
 	for (i = 0; i < num; i++)
 		if (A[i] < 0)
 			S += A[i];
@@ -35,12 +36,16 @@ void main()
 {
 	setlocale(0, "");
 	srand(time(0));
-	int n, m, k, x[60], y[60], z[60], s=0, sum;
-	cout << "Введите n,m,k";
+	int n, m, k, x[60], y[60], z[60], s=0, sum1, sum2, sum3;
+	cout << "Введите n,m,k ";
 	cin >> n >> m >> k;
 	arrayNew(n, x);
 	arrayNew(m, y);
 	arrayNew(k, z);
-	sum = arraySum(arraySum(arraySum(s, n, x), m, y), k, z);
-	cout << sum;
+	sum1 = arraySum(n, x);
+	sum2 = arraySum(m, y);
+	sum3 = arraySum(k, z);
+	cout << "Cумма отриц.элементов массива x = " << sum1 << endl;
+	cout << "Cумма отриц.элементов массива y = " << sum2 << endl; 
+	cout << "Cумма отриц.элементов массива z = " << sum3 << endl;
 }
