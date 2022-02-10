9ff9dd688ef7967c7d38ff31d87740c26b263f84
feroswin <vitalkaivanov009@mail.ru>
2022-02-10 16:20:10 +0300

    лаб 2 C#

---
+++ лабы C#/lab_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time, hours, minutes;
+            Console.Write("Введите количество минут ");
+            time = Convert.ToInt32(Console.ReadLine());
+            hours = time / 60;
+            minutes = time % 60;
+            Console.WriteLine($"Время в необходимом формате {hours} ч {minutes} мин");
+            Console.ReadKey();
+        }
+    }
+}
