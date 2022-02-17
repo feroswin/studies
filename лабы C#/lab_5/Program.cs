6f41daafd8c4911469576df05e4f57617062e99f
feroswin <vitalkaivanov009@mail.ru>
2022-02-17 10:32:09 +0300

    загрузка лабы 5

---
+++ лабы C#/lab_5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static System.Convert;
+using static System.Console;
+using static System.Math;
+
+namespace lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, ost;
+            Write("Введите число x= ");
+            x = ToInt32(ReadLine());
+            ost = x % 8;
+            switch (ost)
+            {
+                case 0:
+                    Write($"Остаток от деления на 8 числа {x} равен нулю");
+                    break;
+                case 1:
+                    Write($"Остаток от деления на 8 числа {x} равен единице");
+                    break;
+                case 2:
+                    Write($"Остаток от деления на 8 числа {x} равен двум");
+                    break;
+                case 3:
+                    Write($"Остаток от деления на 8 числа {x} равен трем");
+                    break;
+                case 4:
+                    Write($"Остаток от деления на 8 числа {x} равен четырем");
+                    break;
+                case 5:
+                    Write($"Остаток от деления на 8 числа {x} равен пяти");
+                    break;
+                case 6:
+                    Write($"Остаток от деления на 8 числа {x} равен шести");
+                    break;
+                case 7: 
+                    Write($"Остаток от деления на 8 числа {x} равен семи");
+                    break;
+            }
+            ReadKey();
+        }
+    }
+}
