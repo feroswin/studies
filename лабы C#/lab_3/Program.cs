e130d8af8e0bedf98525be34f486a187c7e8b01d
feroswin <vitalkaivanov009@mail.ru>
2022-02-17 13:57:50 +0300

    Загрузка 3 лабы с правками

--- лабы C#/lab_3/Program.cs
+++ лабы C#/lab_3/Program.cs
@@ -3,10 +3,7 @@ using static System.Console;
 using static System.Convert;
 using static System.Math;
 
-/*
-    Написать программу, проверяющую, есть ли среди чисел а, b, с хотя бы одна пара
-    взаимно противоположных.
-*/
+
 
 namespace lab_3
 {
@@ -21,25 +18,10 @@ namespace lab_3
             b = ToDouble(ReadLine());
             Write("Введите число c=");
             c = ToDouble(ReadLine());
-            if ((a != b) && (a == Abs(b) || Abs(a) == b))
-            {
-                num_pairs ++;
-                if (b == Abs(c) || Abs(b) == c)
-                    num_pairs++;
-            }
-            else if ((a != c) && (a == Abs(c) || Abs(a) == c))
-            {
-                num_pairs++;
-                if (c == Abs(b) || Abs(c) == b)
-                    num_pairs++;
-            }
-            else if ((b != c) && (b == Abs(c) || Abs(b) == c))
-            {
-                num_pairs++;
-                if (c == Abs(a) || Abs(c) == a)
-                    num_pairs++;
-            }
-            Write($"Количество пар взаимно противоположных чисел равно {num_pairs}");
+            if (a + b == 0 || a + c == 0 || b + c == 0)
+                Write("Взаимно противоположные числа есть");
+            else
+                Write("Взаимно противоположных чисел нет");
             ReadKey();
         }
     }
