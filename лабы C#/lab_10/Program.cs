8c46f88392f91688cebee52f2adaa09dd3083559
feroswin <vitalkaivanov009@mail.ru>
2022-02-27 15:48:17 +0300

    загрузка лабы 10

---
+++ лабы C#/lab_10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+/*  
+    Заполнить матрицу случайными числами. На главной диагонали разместить суммы
+    элементов, которые лежат на соответствующих строках.
+*/
+
+namespace lab_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            double s;
+            Write("Введите длину измерения массива n = ");
+            n = ToInt32(ReadLine());
+            WriteLine("Исходный массив:");
+            double[,] mas = new double[n, n];
+            Random r = new Random();
+            for (i = 0; i < n; i++)
+            {
+                for(j = 0; j < n; j++)
+                {
+                    mas[i, j] = r.Next(-10,15) /2.0;
+                    Write("{0,5:0.0}", mas[i,j]);
+                }
+                WriteLine();
+                WriteLine();
+            }
+            for (i = 0; i < n; i++)
+            {
+                s = 0;
+                for(j = 0; j < n; j++)
+                {
+                    s += mas[i, j];
+                }
+                mas[i, i] = s;
+            }
+            WriteLine("Изменненый массив:");
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++)
+                    Write("{0,5:0.0}", mas[i, j]);
+                WriteLine();
+                WriteLine();
+            }
+        }
+    }
+}
