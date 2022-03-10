40694dc60f39544fad73becfe32e7866a701f7ae
feroswin <vitalkaivanov009@mail.ru>
2022-03-10 16:08:28 +0300

    окончательная загрузка лабы 12

--- лабы C#/lab_12/Program.cs
+++ лабы C#/lab_12/Program.cs
@@ -6,20 +6,24 @@ namespace lab_12
 {
     class Program
     {
+
+
         static void Vvod(int[,] mas, int k)
         {
             WriteLine();
             Random r = new Random();
-            for(int i = 0; i < k; i++)
+            for (int i = 0; i < k; i++)
             {
                 for (int j = 0; j < k; j++)
                 {
                     mas[i, j] = r.Next(-10, 10);
-                    Write("{0,5:0}", mas[i,j]);
+                    Write("{0,5:0}", mas[i, j]);
                 }
                 WriteLine();
             }
         }
+
+
         static void Replace(int[,] mas, int k)
         {
             for (int i = 0; i < k; i++)
@@ -28,48 +32,37 @@ namespace lab_12
                         mas[i, j] = 0;
             WriteLine();
         }
+
+
         static void Output(int[,] mas, int k)
         {
-            for(int i = 0; i < k; i++)
+            for (int i = 0; i < k; i++)
             {
                 for (int j = 0; j < k; j++)
-                    Write("{0,5:0}", mas[i,j]);
+                    Write("{0,5:0}", mas[i, j]);
                 WriteLine();
             }
             WriteLine();
         }
+
+
         static void Main(string[] args)
         {
-            int n, m;
-            Write("Введите размерность первого массива n = ");
-            n = ToInt32(ReadLine());
+            int n;
+            for (int i = 1; i <= 2; i++)
+            {
+                Write($"Введите размерность {i} массива n = ");
+                n = ToInt32(ReadLine());
+                int[,] a = new int[n, n];
+                Write($"{i} массив");
+                Vvod(a, n);
+                Replace(a, n);
 
-            Write("Введите размерность второго массива m = ");
-            m = ToInt32(ReadLine());
-            
-            int[,] a = new int[n,n];
-            
-            WriteLine("Первый масcив");
+                WriteLine($"Измененный {i} массив");
+                WriteLine();
 
-            Vvod(a, n);
-            Replace(a, n);
-
-            WriteLine("Измененный первый массив");
-            WriteLine();
-
-            Output(a, n);
-
-            int[,] b = new int[m, m];
-
-            WriteLine("Второй массив");
-
-            Vvod(b, m);
-            Replace(b, m);
-
-            WriteLine("Измененный второй массив");
-            WriteLine();
-
-            Output(b, m);
+                Output(a, n);
+            }
         }
     }
 }
