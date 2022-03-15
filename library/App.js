da93f46c8ecb3ea41330d24d807039834c03aa04
feroswin <vitalkaivanov009@mail.ru>
2022-03-15 20:40:45 +0300

    library 1 загрузка с недоработанными токенами

--- library/App.js
+++ library/App.js
@@ -1,28 +1,36 @@
 const express = require('express');
-
-const IndexRouter = require('./Routes/index')
+const mongoose = require('mongoose')
+const AuthRouter = require('./Routes/AuthRouter')
+const path = require('path')
 
 const app = express();
 const port = 3000;
 const DB_URL = 'mongodb+srv://user:5841495@library.rrbzq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 app.use(express.json());
-
 // движок шаблонизатора
 app.set('view engine', 'ejs');
 
 // подключение шаблонов
-app.set('/views', express.static('views'));
+app.set('views', path.join(__dirname, 'views'));
 
 // радача статики (сss, png и т.д)
-app.use('/public', express.static('public'))
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Маршруты сайта
 
 //Главная страница
-app.use('/', IndexRouter)
+app.use('/Auth', AuthRouter);
 
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+const start = async () => {
+    try {
+        mongoose.connect(DB_URL);
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+        })
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+start()
