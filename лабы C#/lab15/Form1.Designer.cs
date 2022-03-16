d8a7f543826ef8ac5c9da7221d6d2124412320fd
feroswin <vitalkaivanov009@mail.ru>
2022-03-16 21:12:03 +0300

    Загрузка 15 лабы

---
+++ лабы C#/lab15/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace lab15
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSign = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxDe = new System.Windows.Forms.TextBox();
+            this.textBoxMn = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxSign
+            // 
+            this.textBoxSign.Location = new System.Drawing.Point(176, 116);
+            this.textBoxSign.Name = "textBoxSign";
+            this.textBoxSign.Size = new System.Drawing.Size(27, 23);
+            this.textBoxSign.TabIndex = 2;
+            this.textBoxSign.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxSign_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(20, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Действительная часть";
+            // 
+            // textBoxDe
+            // 
+            this.textBoxDe.Location = new System.Drawing.Point(36, 116);
+            this.textBoxDe.Name = "textBoxDe";
+            this.textBoxDe.Size = new System.Drawing.Size(94, 23);
+            this.textBoxDe.TabIndex = 4;
+            this.textBoxDe.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxDe_KeyPress);
+            // 
+            // textBoxMn
+            // 
+            this.textBoxMn.Location = new System.Drawing.Point(253, 116);
+            this.textBoxMn.Name = "textBoxMn";
+            this.textBoxMn.Size = new System.Drawing.Size(94, 23);
+            this.textBoxMn.TabIndex = 5;
+            this.textBoxMn.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxMn_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(257, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Мнимая часть";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(173, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Знак";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(83, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Результат:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(123, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 28);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Возвести в квадрат";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(92, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(203, 38);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Возведение комплексного \r\nчисла в квадрат";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(162, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 19);
+            this.label6.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 287);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxMn);
+            this.Controls.Add(this.textBoxDe);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSign);
+            this.Name = "Form1";
+            this.Text = "Лабораторная работа № 15";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBoxSign;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxDe;
+        private System.Windows.Forms.TextBox textBoxMn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ лабы C#/lab15/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Convert;
+using static System.Console;
+
+namespace lab15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double De, Mn, result, result1;
+            char sign;
+            try
+            {
+                De = ToDouble(textBoxDe.Text);
+                Mn = ToDouble(textBoxMn.Text);
+                sign = ToChar(textBoxSign.Text);
+                if (sign == '+')
+                {
+                    result = De * De + Mn * Mn * (-1);
+                    result1 = 2 * De * Mn;
+                    label6.Text = result.ToString("f2") + " + " + result1.ToString("f2") + "i"; 
+                } 
+                else
+                {
+                    result = De * De + Mn * Mn * (-1);
+                    result1 = 2 * De * Mn;
+                    label6.Text = result.ToString("f2") + " - " + result1.ToString("f2") + "i";
+                }
+            } catch (FormatException ex)
+            {
+                if (textBoxDe.Text.Length == 0 || (textBoxMn.Text.Length == 0) || (textBoxSign.Text.Length == 0))
+                    MessageBox.Show("Поля должны быть заполнены", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBoxDe_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9')) return;
+            if (e.KeyChar == '.') e.KeyChar = ',';
+            if (e.KeyChar == ',')
+            {
+                if ((textBoxDe.Text.IndexOf(',') != -1) || (textBoxDe.Text.Length == 0))
+                    e.Handled = true;
+                return;
+            }
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                    textBoxSign.Focus();
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void textBoxSign_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar == '+') || (e.KeyChar == '-')) return;
+            if (Char.IsControl(e.KeyChar)) 
+            { 
+                if (e.KeyChar == (char)Keys.Enter)
+                    textBoxMn.Focus();
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void textBoxMn_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9')) return;
+            if (e.KeyChar == '.') e.KeyChar = ',';
+            if (e.KeyChar == ',')
+            {
+                if ((textBoxMn.Text.IndexOf(',') != -1) || (textBoxMn.Text.Length == 0))
+                    e.Handled = true;
+                return;
+            }
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                    button1.Focus();
+                return;
+            }
+            e.Handled = true;
+        }
+    }
+}
