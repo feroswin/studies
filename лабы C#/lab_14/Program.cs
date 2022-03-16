65d86ed1f56d9d24870e2a4f6ae1cf8a18016eb9
feroswin <vitalkaivanov009@mail.ru>
2022-03-16 21:27:31 +0300

    лаб 14

---
+++ лабы C#/lab_14/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+
+namespace lab_14
+{
+
+    struct car
+    {
+        public string brand;
+        public string num;
+        public int year;
+        public int price;
+        public void Vvod()
+        {
+            WriteLine();
+            Write("Введите марку автомобиля: ");
+            brand = ReadLine();
+            Write("Введите номерной знак: ");
+            num = ReadLine();
+            Write("Введите год выпуска: ");
+            year = ToInt32(ReadLine());
+            Write("Введите стоимость: ");
+            price = ToInt32(ReadLine());
+            WriteLine();
+        }
+        public void DisplayInfo()
+        {
+            WriteLine();
+            WriteLine($"Марка автомобиля: {brand}");
+            WriteLine($"Номерной знак: {num.ToUpper()}");
+            WriteLine($"Год выпуска: {year} г.");
+            WriteLine($"Стоимость: {price} руб.");
+            WriteLine();
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n;
+            int year_src;
+            Write("Введите количество автомобилей ");
+            n = ToInt32(ReadLine());
+            car[] cars = new car [n];
+            for (i = 0; i < n; i++)
+            {
+                Write($"{i+1} - автомобиль");
+                cars[i].Vvod();
+            }
+
+            Write("Введите год выпуска автомобиля для поиска ");
+            year_src = ToInt32(ReadLine());
+
+            for (i = 0; i < n; i++)
+            {
+                if(cars[i].year == year_src)
+                {
+                    cars[i].DisplayInfo();
+                }
+            }
+
+        }
+    }
+}
