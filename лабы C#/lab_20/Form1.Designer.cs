f5051d8dba0f988969541e3d833a434c5b32ddb0
feroswin <vitalkaivanov009@mail.ru>
2022-03-30 18:46:38 +0300

    загрузка 20 лабы

---
+++ лабы C#/lab_20/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace lab_20
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(867, 518);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(30, 555);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 611);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ лабы C#/lab_20/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_20
+{
+    public partial class Form1 : Form
+    {
+        Graphics gr;
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Print(int x1, int y1,int W, int H, int single1, int single2)
+        {
+            Random rnd = new Random();
+            int col = rnd.Next();
+            Color color = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+            SolidBrush brush = new SolidBrush(color);
+            Pen pen = new Pen(color, 1.7f);
+            gr.FillPie(brush, x1, y1, W, H, single1, single2);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gr = pictureBox1.CreateGraphics();
+            timer1.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int x1, y1, dopW, dopH, single1, single2;
+            timer1.Interval = 10;
+            int W = pictureBox1.Width;
+            int H = pictureBox1.Height;
+            x1 = r.Next(-W / 2, W);
+            y1 = r.Next(-H / 2, H);
+            single1 = r.Next(-270, 270);
+            single2 = r.Next(-270, 270);
+            dopW = r.Next(W / 4, W/2);
+            dopH = r.Next(H / 4, H/2);
+            Print(x1, y1, dopW, dopH, single1, single2);
+        }
+    }
+}
