b7af80eadf134019f555d3e6f189e977da18e057
feroswin <vitalkaivanov009@mail.ru>
2022-04-13 20:38:09 +0300

    загрузка 22 лабы

---
+++ лабы C#/lab_22/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace lab_22
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(23, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(290, 224);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(365, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Загрузить в файл";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 9F);
+            this.button3.Location = new System.Drawing.Point(365, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 26);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Удалить запись";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 9F);
+            this.button4.Location = new System.Drawing.Point(365, 117);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(153, 26);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Добавить запись";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 9F);
+            this.button5.Location = new System.Drawing.Point(365, 159);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(153, 26);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Считать из файла";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Century Gothic", 9F);
+            this.button6.Location = new System.Drawing.Point(365, 200);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(153, 45);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Сумма очков каждого спортсмена";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(559, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 8;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(23, 282);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(351, 171);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Общее кол-во очков каждого участника";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 491);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ лабы C#/lab_22/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Convert;
+
+namespace lab_22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int i = 0;
+        double s = 0;
+
+
+        // Загрузить в файл
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream filestream = new FileStream("file.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            filestream.SetLength(0);
+            StreamWriter streamwriter = new StreamWriter(filestream);
+            for (int i = 0; i < listBox1.Items.Count; i++)
+                streamwriter.WriteLine(listBox1.Items[i].ToString());
+            streamwriter.Flush();
+            streamwriter.Close();
+            filestream.Close();
+        }
+
+        // Удалить запись
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        // Добавить запись
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.TextLength == 0)
+                    throw new FormatException();
+                string x1 = textBox1.Text;
+                listBox1.Items.Add(x1);
+                textBox1.Text = "";
+            } catch (FormatException)
+            {
+                MessageBox.Show("Поле для добавления записи должно быть заполнено", "Ошибка",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+        }
+        
+        // Считать из файла
+        private void button5_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            s = 0;
+            string stringText;
+            string fileName = "file.txt";
+            TextReader textreader = new StreamReader(fileName);
+            listBox1.Items.Clear();
+            while (textreader.Peek() > -1)
+            {
+                stringText = textreader.ReadLine();
+                listBox1.Items.Add(stringText);
+            }
+            textreader.Close();
+        }
+
+        // Сумма очков каждого спортсмена
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            string[] fields;
+            string stringText;
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                stringText = Convert.ToString(listBox1.Items[i]);
+                fields = stringText.Split(',');
+                sportsmen[i].name = fields[0];
+                sportsmen[i].sum_grade = ToInt32(fields[1]) + ToInt32(fields[2]) + ToInt32(fields[3]);
+                label1.Text += $"Имя:{sportsmen[i].name}  Ср.балл: {sportsmen[i].sum_grade} \n";
+            }
+        }
+
+        struct sportsmens
+        {
+            public string name;
+            public double sum_grade;
+        }
+        sportsmens[] sportsmen = new sportsmens[70];
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= '0') && (e.KeyChar <= '9')) return;
+            if (Char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                    button4_Click(sender, e);
+                return;
+            }
+            if (Char.IsPunctuation(e.KeyChar))
+            {
+                if (e.KeyChar == '.')
+                {
+                    e.KeyChar = ',';
+                    return;
+                }
+                e.Handled = true;
+            }
+        }
+    }
+}
